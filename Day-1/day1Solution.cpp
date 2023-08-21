dd59fa2e8a8d691355a884b39ca2e40602092f1b
Its-Vipin <vkprajapati665@gmail.com>
2023-08-22 00:58:21 +0530

    Day1

--- Day-1/day1Solution.cpp
+++ Day-1/day1Solution.cpp
@@ -3,28 +3,19 @@ using namespace std;
 
 int main(){
     int n;
-    int maxAmount=0;
+    int maxArea = 0;
     cin >> n;
     int height[n];
-    for(int i=0;i<n;i++){
-        cin >> height[i];
+    for(int i = 0; i < n; i++){
+        cin >> height[i]; 
     }
-    int l=0;
-    int r=n-1;
-    while(l<r){
-        int tempArea = ((height[r]>height[l])?height[l]:height[r])*(r-l);
-        maxAmount=(maxAmount>tempArea)?maxAmount:tempArea;
-        if (height[l] < height[r]){
-            l++;
-        }
-        else {
-            r--;
+    for(int i = 0; i < n-1; i++){
+        for(int j = i+1; j < n; j++){
+            int area = ((height[j] > height[i]) ? height[i] : height[j]) * (j - i);
+            maxArea = (maxArea > area) ? maxArea : area;
         }
     }
-    cout << maxAmount;
+    cout << maxArea;
     return 0;
 }
-
-
-
 
